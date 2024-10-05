fbbe321386441111229ba9a7279ea0d81219cd6d
nadriha <hafez.nadhira@gmail.com>
2024-10-06 04:00:48 +0700

    feat: detail crops

--- public/assets/icons/ChartIcon.tsx
+++ public/assets/icons/ChartIcon.tsx
@@ -5,12 +5,12 @@ const ChartIcon = ({ width = 31, height = 31 }: IconInterface) => {
   return (
     <svg width={width} height={height} viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g clip-path="url(#clip0_93_3324)">
-    <path d="M30.97 0H0.25V30.72H30.97V0Z" fill="white" fill-opacity="0.01"/>
-    <path d="M4.08984 3.83997V26.88H27.1298" stroke="black" stroke-width="2.56" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M9.20996 19.2V21.76" stroke="black" stroke-width="2.56" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M14.3301 14.08V21.76" stroke="black" stroke-width="2.56" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M19.4502 3.83997V21.76" stroke="black" stroke-width="2.56" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M24.5703 8.95996V21.76" stroke="black" stroke-width="2.56" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M30.97 0H0.25V30.72H30.97V0Z" fill="white" fillOpacity="0.01"/>
+    <path d="M4.08984 3.83997V26.88H27.1298" stroke="black" strokeWidth="2.56" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M9.20996 19.2V21.76" stroke="black" strokeWidth="2.56" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M14.3301 14.08V21.76" stroke="black" strokeWidth="2.56" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M19.4502 3.83997V21.76" stroke="black" strokeWidth="2.56" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M24.5703 8.95996V21.76" stroke="black" strokeWidth="2.56" strokeLinecap="round" strokeLinejoin="round"/>
     </g>
     <defs>
     <clipPath id="clip0_93_3324">
---
+++ src/modules/CropsDetailModule/index.tsx
@@ -0,0 +1,130 @@
+'use client'
+
+import React, { useState } from 'react'
+import Image from 'next/image'
+import { FilterButton } from '@/components/elements/button/FilterButton'
+import { datahama } from './constant'
+
+export const CropsDetailModule = () => {
+  const [filterCategory, setFilterCategory] = useState('Details')
+  const [subFilter, setSubFilter] = useState('')
+
+  const renderContent = () => {
+    if (filterCategory === 'Details') {
+      return (
+        <>
+          <div className="flex flex-col mx-5">
+            <div className="bg-primary-green px-4 py-5 rounded-[10px] flex flex-col gap-[5px]">
+              <p className="text-section-header text-white">Why?</p>
+              <p className="text-white text-body">{datahama.details.why}</p>
+            </div>
+          </div>
+          <div className="flex flex-col mx-5">
+            <div className="bg-primary-green px-4 py-5 rounded-[10px] flex flex-col gap-[5px]">
+              <p className="text-section-header text-white">When?</p>
+              <p className="text-white text-body">{datahama.details.when}</p>
+            </div>
+          </div>
+        </>
+      )
+    }
+
+    const currentData =
+      filterCategory === 'Pests' ? datahama.pests : datahama.weeds
+    const selectedItem = currentData.find((item) => item.name === subFilter)
+
+    if (selectedItem) {
+      return selectedItem.data.map((item, index) => (
+        <div key={index} className="flex flex-col mx-5">
+          <div className="bg-primary-green px-4 py-5 rounded-[10px] flex flex-col gap-[5px]">
+            <p className="text-section-header text-white">{item.name}</p>
+            <p className="text-white text-body">{item.description}</p>
+          </div>
+        </div>
+      ))
+    } else {
+      return (
+        <div className="flex flex-col mx-5 items-center">
+          <p className="text-section-header text-primary-green">
+            Select a {filterCategory.toLowerCase()}
+          </p>
+          <p className="text-primary-green text-body">
+            Select a {filterCategory.toLowerCase()} to view more information.
+          </p>
+        </div>
+      )
+    }
+  }
+
+  return (
+    <section className="w-full h-full pb-10 relative flex flex-col gap-10">
+      <div className="absolute w-full h-[270px] -z-10">
+        <Image
+          src="/assets/images/CropsDetailPage/backdrop.png"
+          alt="backdrop"
+          fill
+          style={{ objectFit: 'cover', borderRadius: '50px' }}
+        />
+      </div>
+      <div className="flex flex-col z-10 pt-24 px-5 w-full gap-2 text-white">
+        <p className="text-main-header">Strawberry</p>
+        <hr className="w-3/12" />
+        <p className="text-section-header">
+          It’s not the right time to grow strawberries right now.
+        </p>
+        <p className="text-body">
+          Based on the analysis of your geographical conditions, the current
+          climate isn't ideal for growing strawberries.{' '}
+        </p>
+      </div>
+      <section className="flex flex-col gap-3">
+        <div className="px-5 flex items-center gap-2">
+          <FilterButton
+            onClick={() => setFilterCategory('Details')}
+            isActive={filterCategory == 'Details'}
+          >
+            Details
+          </FilterButton>
+          <FilterButton
+            onClick={() => setFilterCategory('Pests')}
+            isActive={filterCategory == 'Pests'}
+          >
+            Pests
+          </FilterButton>
+          <FilterButton
+            onClick={() => setFilterCategory('Weeds')}
+            isActive={filterCategory == 'Weeds'}
+          >
+            Weeds
+          </FilterButton>
+        </div>
+        {filterCategory != 'Details' && (
+          <>
+            <hr className="w-11/12 mx-auto" />
+            <div className="px-5 flex items-center gap-2">
+              {filterCategory == 'Pests' &&
+                datahama.pests.map((item: any) => (
+                  <FilterButton
+                    onClick={() => setSubFilter(item.name)}
+                    isActive={subFilter == item.name}
+                  >
+                    {item.name}
+                  </FilterButton>
+                ))}
+              {filterCategory == 'Weeds' &&
+                datahama.weeds.map((item: any) => (
+                  <FilterButton
+                    onClick={() => setSubFilter(item.name)}
+                    isActive={subFilter == item.name}
+                  >
+                    {item.name}
+                  </FilterButton>
+                ))}
+            </div>
+          </>
+        )}
+        <div className="flex flex-col gap-2 pt-2">{renderContent()}</div>
+      </section>
+    </section>
+  )
+}
